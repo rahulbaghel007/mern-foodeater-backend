01db4df090c4969a2ac32824bdbf8ce21d2c835b
Rahul Baghel <106217726+rahulbaghel007@users.noreply.github.com>
2024-03-05 14:54:10 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,16 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 mongoose.connect(process.env.MONGO_CONFIG as string)
-.then(()=> console.log("Connected to Datacbase"));
+.then(()=> console.log("Connected to Database!"));
 
 const app = express();
 
 const PORT = 7000
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
 
 app.use("/api/my/user", myUserRoute);
 
@@ -18,5 +21,3 @@ app.use("/api/my/user", myUserRoute);
 app.listen(PORT, ()=>{
     console.log(`Server Started @ ${PORT}`)
 })
-
-// export default S
